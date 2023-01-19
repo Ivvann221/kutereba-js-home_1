d8a0b2c3b278f6c566d636dff3abb95032130bba
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2023-01-19 22:12:35 +0200

    4

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,3 @@
-let length = prompt('Введіть довжину сторони квадрата', );
-const formula = 4 * length;
-alert (`Периметр квадрата = ${formula}`)
+let length = prompt('Введіть радіус кола', );
+const formula = 3.14 * Math.pow(length, 2);;
+alert (`Площа кола = ${formula}`)
