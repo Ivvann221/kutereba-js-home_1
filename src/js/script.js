a509878a208c2728420a37d85b0e2860c589330f
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2023-01-21 18:51:56 +0200

    9

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,10 @@
-let moneyInWallet = prompt('Введіть суму грошей у кошильку',);
-if (moneyInWallet > 0) {
-    let price = prompt('Введіть ціну однієї шоколадки',);
-    if(price > 0){
-    const formula = moneyInWallet / price;
-    alert(`Ви зможете купити ${Math.floor(formula)} шоколадок`);
-    alert(`У вас залишилося ${moneyInWallet % price} грн.`);
-    }else {
-        alert("Введіть додатнє число")
-    }
-} else {
-    alert("Введіть додатнє число")
+let number = prompt('Введіть трьох значне число',);
+if (number > 100 && number < 999){
+    alert(number + "-" + number.split("").reverse().join(""));
+}else{
+    alert('Введіть трьох значне число')
 }
+
 
 
 
