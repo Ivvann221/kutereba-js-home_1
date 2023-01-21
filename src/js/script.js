6b7a2cd2bbda4c214c3da3f90b1e828036444356
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2023-01-21 09:36:16 +0200

    6

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-let length = prompt('Введіть відстань у кілометрах між двомамістами', );
-let time= prompt('за скільки годин ви хоче дістатися', );
-const formula = length / time;
-alert (`вам потрібно рухатися зі швидкістю = ${formula} км/год`)
+let dollarr = prompt('Введіть суму в доларах', );
+const euro = 0.92;
+const formula = dollarr * euro;
+alert (` ${formula} Євро`)
