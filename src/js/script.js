ab50ee9b0978885f4cc1f961ad36af2e26c046da
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2023-01-21 18:24:20 +0200

    8

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,21 @@
-
-
-let Vvid =  prompt('Введіть обсяг флешки у ГБ', );
-const memoryGb = 1024;
-const memoryNb = 820;
-const formula = memoryGb * Vvid / memoryNb;
-if (Vvid > 0) {
-    alert(Math.round(formula) + " " + "файлів вміститься на флешку");
-}
-    else{
-        alert("Введіть додатнє число");
+let moneyInWallet = prompt('Введіть суму грошей у кошильку',);
+if (moneyInWallet > 0) {
+    let price = prompt('Введіть ціну однієї шоколадки',);
+    if(price > 0){
+    const formula = moneyInWallet / price;
+    alert(`Ви зможете купити ${Math.floor(formula)} шоколадок`);
+    alert(`У вас залишилося ${moneyInWallet % price} грн.`);
+    }else {
+        alert("Введіть додатнє число")
     }
+} else {
+    alert("Введіть додатнє число")
+}
+
+
+
+
+
 
 
 
