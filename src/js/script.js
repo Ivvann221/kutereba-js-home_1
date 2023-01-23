34019d068b79372102b4eaf9d87a1ccae34b8042
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2023-01-23 11:05:44 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
 let length = prompt('Введіть радіус кола', );
-const formula = 3.14 * Math.pow(length, 2);;
-alert (`Площа кола = ${formula}`)
+const formula = 3.14 * Math.pow(length, 2);
+if(length < 0){
+    alert("Помилка")
+}else{
+    alert (`Площа кола = ${formula}`)
+}
+
