af060115f71f48eb6cded3caf05f04d7cfd8f71a
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2023-01-23 13:04:39 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,8 @@
 let dollarr = prompt('Введіть суму в доларах', );
 const euro = 0.92;
 const formula = dollarr * euro;
-alert (` ${formula} Євро`)
+if(dollarr < 0){
+    alert("Помилка")
+}else{
+    alert (` ${Math.round(formula)} Євро`)
+}
